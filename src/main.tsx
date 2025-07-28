67aaafde8420c7d2ab5f41e065caf54f135e1a9b
iamudyavar <aditya.udyavar@gmail.com>
2025-07-27 21:52:36 -0500

    Fixed analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-// import { Analytics } from "@vercel/analytics/next";
+import { inject } from "@vercel/analytics";
 import App from "./App.tsx";
 import "./index.css";
 
+inject();
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
 		{/* <Analytics /> */}
